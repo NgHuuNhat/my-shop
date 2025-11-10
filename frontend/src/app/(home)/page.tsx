68675367bda0b2b992274a6627c70205da9ada85
Tay Nguyen Huu <tay.nguyenhuu@gmail.com>
2025-11-11 05:07:55 +0700

    update

--- frontend/src/app/(home)/page.tsx
+++ frontend/src/app/(home)/page.tsx
@@ -40,7 +40,6 @@ export default async function HomePage() {
                 Browse Products
               </Link>
             </div>
-            {/* <div className="flex-1 relative w-full h-64 sm:h-80 md:h-96"> */}
             < div className="flex-1 relative min-h-[16rem] w-full h-64 sm:h-80 md:h-96">
               <Image
                 src="https://picsum.photos/600/400?grayscale"
--- frontend/src/app/(home)/products/[id]/page.tsx
+++ frontend/src/app/(home)/products/[id]/page.tsx
@@ -19,19 +19,6 @@ export default async function ProductDetail({ params }: ProductDetailProps) {
     }
 
     return (
-        // <div className='p-4 max-w-4xl mx-auto flex flex-col gap-4'>
-        //     <div className="relative w-full pb-[60%] rounded-lg overflow-hidden">
-        //         <img src={product.thumbnail} alt={product.name} className='absolute inset-0 w-full h-full object-cover rounded-lg' />
-        //     </div>
-        //     <h1 className='text-2xl font-semibold text-gray-900'>{product.name}</h1>
-        //     <p className='text-xl text-gray-700'>{product.price}₫</p>
-        //     {product.description && <p className='text-gray-600'>{product.description}</p>}
-        //     <div className='flex gap-3 mt-4'>
-        //         <button className='flex-1 px-4 py-2 bg-blue-600 text-white font-medium rounded hover:bg-blue-700 transition'>Thêm vào giỏ hàng</button>
-        //         <button className='flex-1 px-4 py-2 bg-green-600 text-white font-medium rounded hover:bg-green-700 transition'>Mua ngay</button>
-        //     </div>
-        // </div>
-
         <div className="p-4 md:p-8 max-w-6xl mx-auto space-y-10">
 
             {/* Breadcrumb */}
