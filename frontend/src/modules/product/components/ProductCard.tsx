6089bae69fec65adcc4492d25ddc52cd46500c78
Tay Nguyen Huu <tay.nguyenhuu@gmail.com>
2025-11-11 04:26:20 +0700

    update

--- frontend/src/modules/product/components/ProductCard.tsx
+++ frontend/src/modules/product/components/ProductCard.tsx
@@ -5,7 +5,10 @@ import Image from 'next/image'
 
 const ProductCard = React.memo(({ product }: ProductCardProps) => {
     return (
-        <Link href={`/products/${product.id}`} className="w-75 group flex flex-col items-start hover:scale-105 transition-transform duration-200 cursor-pointer">
+        <Link
+            href={`/products/${product.id}`}
+            className="group flex flex-col items-start cursor-pointer hover:scale-105 transition-transform duration-200 w-full sm:w-[calc(50%-0.5rem)] md:w-[calc(100%-0.666rem)] lg:w-[calc(100%-0.75rem)]"
+        >
             <div className="relative w-full pb-[100%] overflow-hidden rounded-lg">
                 <Image
                     src={product.thumbnail}
@@ -15,7 +18,9 @@ const ProductCard = React.memo(({ product }: ProductCardProps) => {
                     unoptimized
                 />
             </div>
-            <h2 className="mt-3 text-sm font-medium text-gray-900 truncate">{product.name}</h2>
+            <h2 className="mt-3 text-sm font-medium text-gray-900 truncate">
+                {product.name}
+            </h2>
             <p className="mt-1 text-sm text-gray-500">{product.price}₫</p>
         </Link>
     )
--- frontend/src/modules/product/components/ProductList.tsx
+++ frontend/src/modules/product/components/ProductList.tsx
@@ -16,7 +16,7 @@ export default function ProductList({ products }: ProductListProps) {
     <div className="mt-5 flex-1 flex flex-col items-center justify-center w-full">
       <Pagination items={products} limit={20}>
         {(items: ProductType[]) => (
-          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-6 w-full">
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 w-full max-w-[1400px] mx-auto">
             {items.map((product: ProductType) => (
               <ProductCard key={product.id} product={product} />
             ))}
