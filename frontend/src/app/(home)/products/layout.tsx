ebe47ac4413969fee64b551fc4d2b834f7f4525d
Tay Nguyen Huu <tay.nguyenhuu@gmail.com>
2025-11-14 07:06:16 +0700

    update

--- frontend/src/app/(home)/products/layout.tsx
+++ frontend/src/app/(home)/products/layout.tsx
@@ -1,10 +1,16 @@
+'use client'
 import Search from '@/shared/components/search/Search'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 export default function layout({ children }: any) {
+    const pathname = usePathname()
+
+    const show = pathname === '/products'
+
     return (
         <div className='flex-1 flex flex-col'>
-            <Search />
+            {show && <Search />}
             {children}
         </div>
     )
