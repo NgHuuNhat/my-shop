21d883c38aaf82773eb3f913095c081afcbbd308
Tay Nguyen Huu <tay.nguyenhuu@gmail.com>
2025-11-15 17:57:40 +0700

    update

--- frontend/src/modules/product/components/ProductCard.tsx
+++ frontend/src/modules/product/components/ProductCard.tsx
@@ -5,12 +5,13 @@ import ImageCustom from '@/shared/components/image/ImageCustom'
 
 const ProductCard = (({ product }: ProductCardProps) => {
     return (
-        <Link href={`/products/${product.id}`} className="group flex flex-col items-start cursor-pointer hover:scale-[1.03] transition-transform duration-300 w-full">
-            <div className="relative w-full pb-[120%] overflow-hidden rounded-2xl shadow-lg bg-gray-200">
+        <Link href={`/products/${product.id}`} className="group flex flex-col items-start cursor-pointer overflow-hidden rounded-2xl w-full">
+            <div className="relative w-full pb-[130%] overflow-hidden rounded-2xl shadow-lg bg-gray-200 hover:scale-[1.03] transition-transform duration-300">
                 <ImageCustom
                     src={product.thumbnail || "/images/no-image.png"}
                     alt={product.name || "Product image"}
                 />
+                <div className="absolute inset-0 bg-black/20"></div>
                 <div className='text-white absolute bottom-3 left-3'>
                     <h2 className="mt-4 text-base sm:text-lg md:text-xl font-black line-clamp-2">{product.name}</h2>
                     <p className="mt-1 text-base sm:text-lg md:text-xl font-bold">{product.price}₫</p>
